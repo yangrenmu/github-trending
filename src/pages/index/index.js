5519fcd443f2483b9927e6b5f35083e88719ab2b
yangrenmu <1163393838@qq.com>
2019-03-14 22:35:10 +0800

    小程序init

---
+++ src/pages/index/index.js
@@ -0,0 +1,48 @@
+import Taro, { Component } from '@tarojs/taro'
+import { View, Button, Text } from '@tarojs/components'
+import { connect } from '@tarojs/redux'
+
+import { add, minus, asyncAdd } from '../../actions/counter'
+
+import './index.scss'
+
+
+@connect(({ counter }) => ({
+  counter
+}), (dispatch) => ({
+  add () {
+    dispatch(add())
+  },
+  dec () {
+    dispatch(minus())
+  },
+  asyncAdd () {
+    dispatch(asyncAdd())
+  }
+}))
+class Index extends Component {
+
+    config = {
+    navigationBarTitleText: 'Trending'
+  }
+
+  componentWillReceiveProps (nextProps) {
+    console.log(this.props, nextProps)
+  }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    return (
+      <View className='main'>
+        <View className='description'>See what the GitHub community is most excited about today.</View>
+      </View>
+    )
+  }
+}
+
+export default Index
